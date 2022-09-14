4be321a8522a32539d73b23f2e7096f94c93b6c3
Friedemann <friedemann.drepper@online.de>
2022-09-14 16:30:07 +0200

    added debouncing logic in callback of debouncing timer

--- Core/Src/controller_led.c
+++ Core/Src/controller_led.c
@@ -4,18 +4,20 @@
 
 #include "controller_led.h"
 #include "stdbool.h"
-#include "main.h"
 
 #define NR_OF_LEDS 4
+#define DEBOUNCE_LOOPS 30
 
 
-static volatile bool enabled = false;
+
 static volatile uint8_t loop = 0;
+
 static uint32_t led_pins[] = {LED3_Pin, LED5_Pin, LED6_Pin, LED4_Pin};
 static uint32_t led_ports[] = {LED3_GPIO_Port, LED5_GPIO_Port, LED6_GPIO_Port, LED4_GPIO_Port};
 
 void Led_Module_Init(void) {
     Tim_EnableIRQ(true, TIMER_10);
+    Tim_EnableIRQ(true, TIMER_11);
     Tim_Enable(true, TIMER_10);
 }
 
@@ -24,17 +26,35 @@ void Led_Module_DeInit(void) {
 }
 
 void Exti_0_Callback(void) {
-    if (enabled) {
-        enabled = false;
-        Gpio_Reset_Output_Pin(led_ports[loop], led_pins[loop]); // turn off currently active LED
-    } else {
-        enabled = true;
-    }
-    Tim_Enable(enabled, TIMER_10);
+    Tim_Enable(true, TIMER_11);
 }
 
 void Tim_10_Callback(void) {
     Gpio_Reset_Output_Pin(led_ports[loop], led_pins[loop]);
     loop = (loop + 1) % NR_OF_LEDS;
     Gpio_Set_Output_Pin(led_ports[loop], led_pins[loop]);
+}
+
+void Tim_11_Callback(void) {
+    static volatile bool ledEnabled = false;
+    static volatile uint8_t debounceCounter = 0;
+    volatile bool debounceEnabled = true;
+
+    if (debounceCounter < DEBOUNCE_LOOPS) { // still debouncing
+        if (!Gpio_Is_Input_Pin_Set(USER_BTN_GPIO_Port, USER_BTN_Pin)) {
+            debounceCounter++;
+        } else {
+            debounceEnabled = false;
+        }
+    } else { // signal clear after debouncing
+        if (ledEnabled) {
+            ledEnabled = false;
+            Gpio_Reset_Output_Pin(led_ports[loop], led_pins[loop]); // turn off currently active LED
+        } else {
+            ledEnabled = true;
+        }
+        Tim_Enable(ledEnabled, TIMER_10);
+        debounceEnabled = false;
+    }
+    Tim_Enable(debounceEnabled, TIMER_11);
 }
