fc6e762d366eab3915c136b65fc585f886cc1520
Friedemann <friedemann.drepper@online.de>
2022-09-14 10:06:31 +0200

    added pinout header

---
+++ Core/Inc/pinout.h
@@ -0,0 +1,19 @@
+//
+// Created by Friedemann Drepper on 14.09.22.
+//
+
+#ifndef YS_EXERCISE_WEEK3_PINOUT_H
+#define YS_EXERCISE_WEEK3_PINOUT_H
+
+#include "gpio_subsystem.h"
+
+#define LED4_Pin Gpio_Pin_12
+#define LED4_GPIO_Port Gpio_Port_D
+#define LED3_Pin Gpio_Pin_13
+#define LED3_GPIO_Port Gpio_Port_D
+#define LED5_Pin Gpio_Pin_14
+#define LED5_GPIO_Port Gpio_Port_D
+#define LED6_Pin Gpio_Pin_15
+#define LED6_GPIO_Port Gpio_Port_D
+
+#endif //YS_EXERCISE_WEEK3_PINOUT_H
