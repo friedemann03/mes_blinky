c9043076e24f6778cbf7f62cacde976dafb1a3d2
Friedemann <friedemann.drepper@online.de>
2022-09-15 13:08:28 +0200

    added basic IO retargeting to use printf

---
+++ Core/Inc/retarget.h
@@ -0,0 +1,13 @@
+//
+// Created by Friedemann Drepper on 15.09.22.
+//
+
+#ifndef YS_EXERCISE_WEEK3_RETARGET_H
+#define YS_EXERCISE_WEEK3_RETARGET_H
+
+#include "usart.h"
+#include "stdio.h"
+
+void RetargetInit(UART_HandleTypeDef *huart);
+
+#endif //YS_EXERCISE_WEEK3_RETARGET_H
