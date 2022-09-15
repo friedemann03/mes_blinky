f3e6b101e18e6469829aedbf7cd95084199ae3da
Friedemann <friedemann.drepper@online.de>
2022-09-15 14:00:33 +0200

    added logging to subsystems and led controller

--- Core/Src/controller_led.c
+++ Core/Src/controller_led.c
@@ -4,6 +4,7 @@
 
 #include "controller_led.h"
 #include "stdbool.h"
+#include "log_module.h"
 
 #define NR_OF_LEDS 1
 #define DEBOUNCE_LOOPS 25
@@ -16,9 +17,11 @@ static uint32_t led_pins[] = {LED2_Pin};
 static uint32_t led_ports[] = {LED2_GPIO_Port};
 
 void Led_Controller_Init(void) {
+    Log_Message(LOG_LVL_SYSTEM, "LED Controller Starting.\n");
     Tim_EnableIRQ(true, TIMER_10);
     Tim_EnableIRQ(true, TIMER_11);
     Tim_Enable(true, TIMER_10);
+    Tim_Enable(false, TIMER_11);
 }
 
 void Led_Controller_DeInit(void) {
@@ -26,11 +29,13 @@ void Led_Controller_DeInit(void) {
 }
 
 void Exti_15_10_Callback(void) {
+    Log_Message(LOG_LVL_INFO, "Button pressed\n");
     Tim_Enable(true, TIMER_11);
 }
 
 void Tim_10_Callback(void) {
     Gpio_Toggle_Output_Pin(led_ports[0], led_pins[0]);
+    Log_Message(LOG_LVL_INFO, "Toggle: Port A output register(ODR): %x\n", GPIOA->ODR);
 }
 
 void Tim_11_Callback(void) {
