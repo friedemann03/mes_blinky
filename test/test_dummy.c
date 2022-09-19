b7ca2c9ddb5816771942c6540a97e67b8a1e06eb
Friedemann <friedemann.drepper@online.de>
2022-09-19 11:59:26 +0200

    added small dummy test for framework assertion

---
+++ test/test_dummy.c
@@ -0,0 +1,12 @@
+//
+// Created by Friedemann Drepper on 19.09.22.
+// Simple Test file to make sure the test framework is working.
+//
+
+#include "../vendor/ceedling/vendor/unity/src/unity.h"
+#include "stdbool.h"
+
+void test_dummy_shouldFail(void) {
+    TEST_ASSERT(true);
+}
+
